6fb607627627fee733b9435bd2a53442cb719aa9
Eric Citaire <eric.citaire@gmail.com>
2014-08-04 22:57:24 +0200

    add my-jenkins cookbook

---
+++ my-jenkins/CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of my-jenkins
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ my-jenkins/metadata.rb
@@ -0,0 +1,8 @@
+name             'my-jenkins'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures my-jenkins'
+long_description 'Installs/Configures my-jenkins'
+version          '0.1.0'
+
